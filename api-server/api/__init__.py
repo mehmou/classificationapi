6200aac17d2b530d4c672be2657df09c087f5818
lyleokoth <okothlyleochieng@gmail.com>
2022-03-17 22:19:16 +0300

    feat: copy trained model to the backend folder

--- api-server/api/__init__.py
+++ api-server/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from api import models, routes
+from api import models, routes, config
--- api-server/api/routes.py
+++ api-server/api/routes.py
@@ -1,7 +1,16 @@
+from crypt import methods
 from api import app
+import os
 
 
 @app.route('/api')
 @app.route('/')
 def api_home():
     return 'Hello from ml api!'
+
+@app.route('/api/ml', methods=['GET'])
+def model_prediction():
+    model_path = os.path.split(os.getcwd())[1]
+    model_path = os.path.join('.', '..', model_path)
+    print(os.listdir(model_path))
+    return model_path
